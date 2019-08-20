3bc6b423db994a144446d49a212b4ba79d3e40f8
Aruna <Aug@2019>
2019-08-20 13:43:26 +0530

    first commit

---
+++ src/main/java/com/cg/springbootdemoapplication/myspringbootapp/controller/SpringController.java
@@ -0,0 +1,13 @@
+package com.cg.springbootdemoapplication.myspringbootapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "My springboot application";
+	}
+}
